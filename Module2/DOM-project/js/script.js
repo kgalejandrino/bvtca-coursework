3b5cdecc1cc85d4ec31a8eecf49b2bebeefe748d
Kevin Alejandrino <kgalejandrino@gmail.com>
2021-12-09 16:00:51 -0800

    Finished clear task functionality, which clears all the task in the list

--- Module2/DOM-project/js/script.js
+++ Module2/DOM-project/js/script.js
@@ -1,10 +1,17 @@
 /*--- Elements ---*/
 const taskInput = document.getElementById('task');
+const filterInput = document.getElementById('filter');
 const taskLists = document.getElementById('task-lists');
 const btnAdd = document.getElementById('btn-add');
+const btnClear = document.getElementById('btn-clear');
 
 /*--- Function --- */
+// Function: Add task input to the list
 const addTask = () => {
+    // If input is '' or if task is already added,
+    // don't execute the function
+    if(inputValidation()) return;
+
     // Create a new li element
     const task = document.createElement('li');
     // Add a class to the li element
@@ -16,29 +23,114 @@ const addTask = () => {
 
     // Create a text node using the input value from input (new task) 
     const inputValue = document.createTextNode(taskInput.value);
+
     // Append input task & icon to li
     task.appendChild(inputValue);
     task.appendChild(icon);
 
     // Append li(task) to ul
     taskLists.appendChild(task);
+
+    saveTask();
+    // Add task to localstorage
+    clearInput();
+    
 }
 
-
-/*--- Event Handlers ---*/
-// Event Clicked: Add task when button 'Add Task' is clicked 
-btnAdd.addEventListener('click', addTask);
-
-// Event Keyup: Add a task when enter key is pressed
-document.addEventListener('keyup', (event) => {
-    if(event.key === 'Enter') {
-        addTask();
-    }
-});
-
-// Event Clicked: Remove task
-taskLists.addEventListener('click', function(e) {
+// Function: Remove task from the list 
+const removeTask = (e) => {
     if(e.target.classList.contains('icon-close')) {
         e.target.parentNode.remove();
+        saveTask();
     }
-})
+}
+
+// Function: Filter Task
+const filterTask = () => {
+    const inputValue = filterInput.value.toLowerCase();
+    const li = document.querySelectorAll('li');
+
+    li.forEach(item => {
+        if(item.textContent.toLowerCase().includes(inputValue)) {
+            item.style.display = 'flex';
+        } else {
+            item.style.display = 'none';
+        }
+    })
+}
+
+// Function: Clear Task 
+const clearTask = () => {
+    localStorage.clear();
+
+    while(taskLists.firstChild) {
+        taskLists.firstChild.remove();
+    }
+}
+
+// Function: Validates input
+const inputValidation = () => {
+    const li = document.querySelectorAll('li');
+
+    const liArr = Array.from(li);
+    const taskAdded = liArr.find(item => item.textContent === taskInput.value);
+
+    if(taskInput.value === '') {
+        clearInput();
+        alert('Please add a task.');
+        return true;
+    } else if(taskAdded) {
+        clearInput();
+        alert('Task added already.')
+        return true;
+    }
+    return false;
+}
+
+// Function: Clear input field
+const clearInput = () => taskInput.value = ''; 
+
+// Function: Save task to the local storage
+const saveTask = () => {
+    const allTask = [];
+
+    const li = document.querySelectorAll('li');
+    li.forEach(item => {
+        allTask.push(item.innerHTML);
+    })
+
+    localStorage.setItem('task', JSON.stringify(allTask));
+}
+
+// Function: Fetched data from storage and display task list
+const loadTaskFromStorage = () => {
+    const storedValue = JSON.parse(localStorage.getItem('task'));
+
+    if(storedValue) {
+        storedValue.map(item => {
+            // Create a new li element
+            const task = document.createElement('li');
+            // Add a class to the li element
+            task.className = 'list-group-item';
+    
+            task.innerHTML = item;
+            // Append li(task) to ul
+            taskLists.appendChild(task);
+        })
+    }
+}
+
+loadTaskFromStorage();
+
+/*--- Event Handlers ---*/
+// Event Click: Add task to the list
+btnAdd.addEventListener('click', addTask);
+
+// Event Click: Remove task from the list
+taskLists.addEventListener('click', (event) => removeTask(event));
+
+// Event Keyup: Filter task on input change 
+filterInput.addEventListener('keyup', filterTask);
+
+// Event Click: Clear all task from the list
+btnClear.addEventListener('click', clearTask);
